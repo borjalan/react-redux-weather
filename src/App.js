e5c9f2c08e6bc409a6ad434a1b859133c713c483
Borja Landaburu <borja.landaburu@vipera.com>
2020-12-27 18:06:18 +0100

    Code to refactor with Redux

---
+++ src/App.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-console */
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Paper from 'material-ui/Paper';
+import AppBar from 'material-ui/AppBar';
+import LocationList from './components/LocationList';
+import ForecastExtended from './components/ForecastExtended';
+
+import './App.css';
+
+const cities = [
+  'Buenos Aires,ar',
+  'Washington,us',
+  'Bogota,co',
+  'Mexico,mx',
+  'Madrid,es'
+];
+
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { city: null };
+  }
+
+  handleSelectedLocation = city => {
+    this.setState({ city });
+    console.log(`handleSelectedLocation ${city}`);
+  }
+  
+  render() {
+    const { city } = this.state;
+    return (
+
+      <MuiThemeProvider>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <AppBar title="Weather App" />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList cities={cities} 
+              onSelectedLocation={this.handleSelectedLocation} ></LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+              <Paper zDepth={4}>
+                <div className='detail'>
+                  {
+                    city && 
+                    <ForecastExtended city={city}></ForecastExtended>
+                  }
+
+                </div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
