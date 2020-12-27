787a2ffb70b815adb8068836757d84849823382b
Borja Landaburu <borja.landaburu@vipera.com>
2020-12-27 19:07:08 +0100

    simple example redux devtool action

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-/* eslint-disable no-console */
+// vendors
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import Paper from 'material-ui/Paper';
-import AppBar from 'material-ui/AppBar';
-import LocationList from './components/LocationList';
-import ForecastExtended from './components/ForecastExtended';
+import { createStore } from 'redux';
 
+// components
+import ForecastExtended from './components/ForecastExtended';
+import LocationList from './components/LocationList';
+
+// material-ui
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+
+// styles
 import './App.css';
 
-const cities = [
-  'Buenos Aires,ar',
-  'Washington,us',
-  'Bogota,co',
-  'Mexico,mx',
-  'Madrid,es'
-];
+//constants
+const cities = ['Madrid', 'Barcelona', 'Buenos Aires', 'Washington', 'Bogota', 'Mexico'];
 
-
-
+const store = createStore(() => {},
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 class App extends Component {
-
   constructor() {
     super();
     this.state = { city: null };
@@ -29,12 +29,12 @@ class App extends Component {
   handleSelectedLocation = city => {
     this.setState({ city });
     console.log(`handleSelectedLocation ${city}`);
-  }
-  
+    store.dispatch({ type: 'setCity', value: city });
+  };
+
   render() {
     const { city } = this.state;
     return (
-
       <MuiThemeProvider>
         <Grid>
           <Row>
@@ -44,17 +44,15 @@ class App extends Component {
           </Row>
           <Row>
             <Col xs={12} md={6}>
-              <LocationList cities={cities} 
-              onSelectedLocation={this.handleSelectedLocation} ></LocationList>
+              <LocationList
+                cities={cities}
+                onSelectedLocation={this.handleSelectedLocation}
+              ></LocationList>
             </Col>
             <Col xs={12} md={6}>
               <Paper zDepth={4}>
-                <div className='detail'>
-                  {
-                    city && 
-                    <ForecastExtended city={city}></ForecastExtended>
-                  }
-
+                <div className="detail">
+                  {city && <ForecastExtended city={city}></ForecastExtended>}
                 </div>
               </Paper>
             </Col>
